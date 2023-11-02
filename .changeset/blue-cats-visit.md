ad2b7c6ac5febfb71580468f34a82d064dd6c72b
charlbestercpt <111444412+charlbestercpt@users.noreply.github.com>
2023-11-02 15:30:07 +0200

    Testing NPM

---
+++ .changeset/blue-cats-visit.md
@@ -0,0 +1,5 @@
+---
+'@cityrewards/citypassports': minor
+---
+
+Testing NPM
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Charl BesterTest';
+  const name = 'Charl BesterNPM';
   greetUser(name);
 });
